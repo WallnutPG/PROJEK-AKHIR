ad481cb86ca488a8358e14c6cad2cfda0ae16532
WallnutPG <113717223+WallnutPG@users.noreply.github.com>
2023-01-12 12:47:59 +0700

    Create getFile.cpp

---
+++ getFile.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+int main(){
+    ofstream.Android;
+  
+  Android.open ("data" && "com.whatsapp" ios::app){
+    if(!Android.fail()){
+      std::<h1>Error!</h1>
+        } else {
+      Android.downloads() || Android.open(){
+        downloads"cd Downloads" [file.zip] @get.downloads_Android.downloads()
+          }
+      
+      system("cls");
+      syatem("open");
+      
+      std::print << endl;
+      
+      retrun 0;
+}
